1cbb63bdf1e52e91a7c1a8b1538add9dd9088cd9
styse01 <shmadad0@gmail.com>
2023-03-16 23:42:58 +0330

    Add swagger tags

--- laravel_shop/app/Http/Controllers/Controller.php
+++ laravel_shop/app/Http/Controllers/Controller.php
@@ -3,10 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+/**
+ * @OA\Info(
+ *      version="1.0.1",
+ *      title="Laravel shop",
+ *      description="API of a simple shop website."
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Local Machine"
+ * )
+ * 
+ * 
+ * @OA\Tag(
+ *     name="Providers",
+ *     description="v1"
+ * )
+ * 
+ * @OA\Tag(
+ *     name="Users",
+ *     description="v1"
+ * )
+ * 
+ * @OA\Tag(
+ *     name="Products",
+ *     description="v1"
+ * )
+ *
+ * @OA\Tag(
+ *     name="Categories",
+ *     description="v1"
+ * )
+ * 
+ * @OA\Tag(
+ *     name="Brands",
+ *     description="v1"
+ * )
+ * 
+ * @OA\Tag(
+ *     name="Geo",
+ *     description="v1"
+ * )
+ * 
+*/
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
