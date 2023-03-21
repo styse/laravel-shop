1fefe7e4fc0e4b85b29274fc0935699ba86e3a9c
styse01 <shmadad0@gmail.com>
2023-03-21 21:14:33 +0330

    Create UsersTableSeeder

---
+++ laravel_shop/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'phone_number' => '+9812345678',
+
+        ]);
+    }
+}
