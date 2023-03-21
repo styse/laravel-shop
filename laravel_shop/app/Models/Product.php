06c427fbbe60bcd83497bec89e0095ab85db1666
styse01 <shmadad0@gmail.com>
2023-03-21 20:50:32 +0330

    Create product model

---
+++ laravel_shop/app/Models/Product.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @OA\Schema(
+ *     title="Product",
+ *     description="Product Model",
+ *     @OA\Xml(
+ *         name="Product"
+ *     )
+ * )
+ */
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id', 'name', 'description', 'slug' , 'stock'];
+
+    
+    /** 
+     * @OA\Property(
+     *     title="Id",
+     *     description="Id of the product",
+     *     format="integer",
+     *     example="1"
+     * )
+     *
+     * @var integer
+     */
+    private $id;
+
+     /** 
+     * @OA\Property(
+     *     title="name",
+     *     description="products name",
+     *     format="string",
+     *     example="asus laptop"
+     * )
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @OA\Property(
+     *      title="description",
+     *      description="products description",
+     *      example="Veniam excepteur ex excepteur aliquip aute sunt minim et."
+     * )
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @OA\Property(
+     *      title="Slug",
+     *      description="slug for a product",
+     *      example="asuse-laptop-274h8fk"
+     * )
+     *
+     * @var string
+     */
+    public $slug;
+
+    /**
+     * @OA\Property(
+     *      title="NationalCode Verified",
+     *      description="Is this product stocked?",
+     *      example="false"
+     * )
+     *
+     * @var boolean
+     */
+    public $stock;
+}
