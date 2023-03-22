9c04fb8f22d2485cd51854d431440406f8ab54f3
styse01 <shmadad0@gmail.com>
2023-03-23 00:55:17 +0430

    Change Products seeder file name

--- laravel_shop/database/seeders/DatabaseSeeder.php
+++ laravel_shop/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {   
         $this->call([
             UsersTableSeeder::class,
-            ProductsTableSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
