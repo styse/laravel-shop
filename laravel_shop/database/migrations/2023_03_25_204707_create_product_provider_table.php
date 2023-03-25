8e469135a793a3f7a8edda6f2b09fd830c3f8412
styse01 <shmadad0@gmail.com>
2023-03-26 01:38:32 +0430

    Create product_provider table

---
+++ laravel_shop/database/migrations/2023_03_25_204707_create_product_provider_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_provider', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')
+                    ->references('id')
+                    ->on('products');
+            $table->foreignId('provider_id')
+                    ->references('id')
+                    ->on('providers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_provider');
+    }
+};
