3846a7df92587ab9d20401589b7cc936754a6bc6
styse01 <shmadad0@gmail.com>
2023-03-25 23:04:04 +0430

    Create providerResource

--- laravel_shop/app/Http/Resources/ProviderResource.php
+++ laravel_shop/app/Http/Resources/ProviderResource.php
@@ -1,14 +1,19 @@
 <?php
+
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProviderResource extends JsonResource
 {
-    public function toArray($request)
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
     {
-        return parent::toArray($request); 
+        return parent::toArray($request);
     }
 }
-
-?>
