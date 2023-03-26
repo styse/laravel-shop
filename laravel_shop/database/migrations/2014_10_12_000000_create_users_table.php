35330413676e3bcde0e3a5e70068c1daa7e2584a
styse01 <shmadad0@gmail.com>
2023-03-26 23:55:48 +0430

    Add api_token and remember_token to users table

--- laravel_shop/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel_shop/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_number');
-            $table->rememberToken();
+            $table->char('api_token')->nullable();
+            $table->string('remember_token')->nullable();
             $table->timestamps();
         });
     }
