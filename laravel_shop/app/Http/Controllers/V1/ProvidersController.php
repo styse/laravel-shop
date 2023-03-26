812f0fe242050b236e93f947415d06685e04f3ae
styse01 <shmadad0@gmail.com>
2023-03-26 22:17:53 +0430

    Rename ProvidersController class name

--- laravel_shop/app/Http/Controllers/V1/ProvidersController.php
+++ laravel_shop/app/Http/Controllers/V1/ProvidersController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Gate as FacadesGate;
 
-class ProvidersController extends Controller
+class providersController extends Controller
 {
 
     /**
