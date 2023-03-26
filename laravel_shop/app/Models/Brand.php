384d46afc4acef2c9911ffb876a953a1ab624eb1
styse01 <shmadad0@gmail.com>
2023-03-27 01:02:55 +0430

    Create Brand model

---
+++ laravel_shop/app/Models/Brand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @OA\Schema(
+ *     title="Brand",
+ *     description="Brand Model",
+ *     @OA\Xml(
+ *         name="Brand"
+ *     )
+ * )
+ */
+class Brand extends Model
+{
+    use HasFactory;
+        
+    protected $fillable=['id', 'name', 'slug'];
+     
+        
+    /** 
+     * @OA\Property(
+     *     title="ID",
+     *     description="ID of the brand",
+     *     format="integer",
+     *     example=1
+     * )
+     *
+     * @var integer
+     */
+    private $id;
+
+    /** 
+     * @OA\Property(
+     *     title="Name",
+     *     description="Name of the brand",
+     *     format="string",
+     *     example="Asus"
+     * )
+     *
+     * @var string
+     */
+    public $name;
+    
+    /**
+     * @OA\Property(
+     *      title="Slug",
+     *      description="slug of the brand",
+     *      example="asus-laptops-23t45tge5674ewedfk"
+     * )
+     *
+     * @var string
+     */
+    public $slug;
+}
