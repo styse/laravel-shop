6d5511ad861eb4542b865daf5568eae9f677e725
styse01 <shmadad0@gmail.com>
2023-06-05 17:37:15 +0330

    Add searchable

--- laravel_shop/app/Models/Brand.php
+++ laravel_shop/app/Models/Brand.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 /**
  * @OA\Schema(
@@ -17,7 +18,8 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
-        
+    use Searchable;
+
     protected $fillable=['id', 'name', 'slug'];
      
         
